5e15c58dde778a3b151d404fab0b7ab550f84f8f
Legend Chen <s884812@gmail.com>
2019-03-22 21:20:48 +0800

    update
    修改一些模版

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_system/system.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_system/system.lua
@@ -20,7 +20,7 @@ s.anonymous = true
 s.addremove = false
 
 s:tab("general",  translate("General Settings"))
-s:tab("logging",  translate("Logging"))
+--s:tab("logging",  translate("Logging"))
 s:tab("language", translate("Language and Style"))
 
 if has_zram then s:tab("zram", translate("ZRam Settings")) end
@@ -67,67 +67,7 @@ end
 -- Logging
 --
 
-o = s:taboption("logging", Value, "log_size", translate("System log buffer size"), "kiB")
-o.optional    = true
-o.placeholder = 16
-o.datatype    = "uinteger"
 
-o = s:taboption("logging", Value, "log_ip", translate("External system log server"))
-o.optional    = true
-o.placeholder = "0.0.0.0"
-o.datatype    = "ip4addr"
-
-o = s:taboption("logging", Value, "log_port", translate("External system log server port"))
-o.optional    = true
-o.placeholder = 514
-o.datatype    = "port"
-
-o = s:taboption("logging", ListValue, "log_proto", translate("External system log server protocol"))
-o:value("udp", "UDP")
-o:value("tcp", "TCP")
-
-o = s:taboption("logging", Value, "log_file", translate("Write system log to file"))
-o.optional    = true
-o.placeholder = "/tmp/system.log"
-
-o = s:taboption("logging", ListValue, "conloglevel", translate("Log output level"))
-o:value(8, translate("Debug"))
-o:value(7, translate("Info"))
-o:value(6, translate("Notice"))
-o:value(5, translate("Warning"))
-o:value(4, translate("Error"))
-o:value(3, translate("Critical"))
-o:value(2, translate("Alert"))
-o:value(1, translate("Emergency"))
-
-o = s:taboption("logging", ListValue, "cronloglevel", translate("Cron Log Level"))
-o.default = 8
-o:value(5, translate("Debug"))
-o:value(8, translate("Normal"))
-o:value(9, translate("Warning"))
-
-
---
--- Zram Properties
---
-if has_zram then
-	o = s:taboption("zram", Value, "zram_size_mb", translate("ZRam Size"), translate("Size of the ZRam device in megabytes"))
-	o.optional    = true
-	o.placeholder = 16
-	o.datatype    = "uinteger"
-	
-	o = s:taboption("zram", ListValue, "zram_comp_algo", translate("ZRam Compression Algorithm"))
-	o.optional    = true
-	o.placeholder = lzo
-	o:value("lzo", "lzo")
-	o:value("lz4", "lz4")
-	o:value("deflate", "deflate")
-	
-	o = s:taboption("zram", Value, "zram_comp_streams", translate("ZRam Compression Streams"), translate("Number of parallel threads used for compression"))
-	o.optional    = true
-	o.placeholder = 1
-	o.datatype    = "uinteger"
-end
 
 
 --
@@ -174,76 +114,6 @@ end
 -- NTP
 --
 
-if has_ntpd then
 
-	-- timeserver setup was requested, create section and reload page
-	if m:formvalue("cbid.system._timeserver._enable") then
-		m.uci:section("system", "timeserver", "ntp",
-			{
-                	server = { "0.openwrt.pool.ntp.org", "1.openwrt.pool.ntp.org", "2.openwrt.pool.ntp.org", "3.openwrt.pool.ntp.org" }
-			}
-		)
-
-		m.uci:save("system")
-		luci.http.redirect(luci.dispatcher.build_url("admin/system", arg[1]))
-		return
-	end
-
-	local has_section = false
-	m.uci:foreach("system", "timeserver", 
-		function(s) 
-			has_section = true 
-			return false
-	end)
-
-	if not has_section then
-
-		s = m:section(TypedSection, "timeserver", translate("Time Synchronization"))
-		s.anonymous   = true
-		s.cfgsections = function() return { "_timeserver" } end
-
-		x = s:option(Button, "_enable")
-		x.title      = translate("Time Synchronization is not configured yet.")
-		x.inputtitle = translate("Set up Time Synchronization")
-		x.inputstyle = "apply"
-
-	else
-		
-		s = m:section(TypedSection, "timeserver", translate("Time Synchronization"))
-		s.anonymous = true
-		s.addremove = false
-
-		o = s:option(Flag, "enable", translate("Enable NTP client"))
-		o.rmempty = false
-
-		function o.cfgvalue(self)
-			return sys.init.enabled("sysntpd")
-				and self.enabled or self.disabled
-		end
-
-		function o.write(self, section, value)
-			if value == self.enabled then
-				sys.init.enable("sysntpd")
-				sys.call("env -i /etc/init.d/sysntpd start >/dev/null")
-			else
-				sys.call("env -i /etc/init.d/sysntpd stop >/dev/null")
-				sys.init.disable("sysntpd")
-			end
-		end
-
-
-		o = s:option(Flag, "enable_server", translate("Provide NTP server"))
-		o:depends("enable", "1")
-
-
-		o = s:option(DynamicList, "server", translate("NTP server candidates"))
-		o.datatype = "host(0)"
-		o:depends("enable", "1")
-
-		-- retain server list even if disabled
-		function o.remove() end
-
-	end
-end
 
 return m
